8bbc763321fb8b958ea5c6f9aa2f52a987ae7cb3
성현Maria 김 <globalkmaria@gmail.com>
2021-07-05 03:31:43 +0900

    random data

---
+++ src/data.js
@@ -0,0 +1,235 @@
+export const detail_data = [
+  {
+    id: 1,
+    title: 'OneNote vs. Evernote: A personal take on two great note ...',
+    siteTags: [],
+    moreInfo: [
+      [
+        'Description',
+        'OneNote: A great way to get organized ... OneNote is very much a full-blown application. It lets you create simple or complex notes from scratch, ...',
+      ],
+      ['Authors', 'Preston Gralla'],
+    ],
+    liner_highlight:
+      'However, the company changed course in late 2019, announcing that it will continue to add new features to desktop version of OneNote and that the desktop version will once again be included with Office 365 as of March 2020',
+    siteimg: '/assests/site_img/site1.jpeg',
+    linkImg: '/assests/link_img/link1.png',
+    link: 'www.computerworld.com',
+  },
+  {
+    id: 2,
+    title: '20 macOS Shortcuts to Make You Work Like a Genius',
+    siteTags: ['apple', 'gadgets', 'hacks', 'productivity', 'technology'],
+    moreInfo: [
+      [
+        'Description',
+        "For those who wear a lot of hats, your computer's operating system can help you switch tasks quickly",
+      ],
+      ['Authors', 'Lydia Belanger'],
+    ],
+    liner_highlight:
+      'In Finder, you can secondary click a file and assign it a color “Tag.” When you click that Tag on the left sidebar of Finder, all folders or files with that Tag will appear. You can also drag a Tag group (what’s called a "Stack") from the Finder sidebar down to your Dock (row of icons) for easier access',
+    siteimg: '/assests/site_img/site2.jpeg',
+    linkImg: '/assests/link_img/link2.jpeg',
+    link: 'www.entrepreneur.com',
+  },
+  {
+    id: 3,
+    title: 'The Ultimate Guide to Google Docs for Writers (+ workflow ...',
+    siteTags: ['google'],
+    moreInfo: [
+      [
+        'Description',
+        'Update: Google Docs now has a built-in outliner. When you open it from Tools > Document Outline View > Show document outline, it will pick up any heading ...',
+      ],
+      ['Authors', 'http://facebook.com/ladywriters'],
+    ],
+    liner_highlight:
+      'Update: Google Docs now has a built-in outliner. When you open it from Tools > Document Outline View > Show document outline, it will pick up any heading ...',
+    siteimg:
+      'https://www.eadeverell.com/wp-content/uploads/googledocs-800x1150.jpg',
+    linkImg:
+      'https://www.eadeverell.com/wp-content/uploads/2013/10/pic3-200x200.jpg',
+    link: 'www.eadeverell.com',
+  },
+  {
+    id: 4,
+    title: 'Beyond Spaced Repetition: Δ in Roam Research - iantay.dev',
+    siteTags: [],
+    moreInfo: [
+      [
+        'Description',
+        '10 Oct 2020. tags: spaced repetition roam. The Delta (Δ) function in Roam is ... The Delta (Δ) function in Roam is probably the most powerful feature in Roam ...',
+      ],
+    ],
+    liner_highlight:
+      'How I think of it is that it works kind of like time travel with clones. When you click on the Δ button for a block, the original block is sent forward in time. In its place a “clone” is created. This clone is basically a copy of the original block that links back to the original. If you have used the Δ button once for a given block, the links...',
+    siteimg: '',
+    linkImg: 'https://getliner.com/src/images/favicon.svg',
+    link: 'www.iantay.dev',
+  },
+  {
+    id: 5,
+    title:
+      'Why are the tags in your template without hashtags? · Discussion #35 · BryanJenksCommunity/FAQ',
+    siteTags: [],
+    moreInfo: [
+      [
+        'Description',
+        'Where I answer questions . Contribute to BryanJenksCommunity/FAQ development by creating an account on GitHub.',
+      ],
+    ],
+    liner_highlight: `The way I build MOC's with the [[tags]] links that don't exist mean I don't need to manage/track anything in regards to promoting a [[tag]] to a MOC I can just see a big red node on my graph, it's VISUALLY apparent`,
+    siteimg: 'https://github.com/opengraph.png',
+    linkImg: 'https://github.githubassets.com/favicons/favicon.svg',
+    link: 'github.com',
+  },
+];
+export const trend_tag = [
+  'upload',
+  'sharing',
+  'video',
+  'news',
+  'free',
+  'education',
+  'ncbi',
+  'health',
+  'medline',
+  'nlm',
+  'national',
+];
+export const trending_pages = [
+  {
+    id: 1,
+    tags: ['books', 'david foster wallace', 'education'],
+    title: 'How to Remember What You Read - Farnam Street',
+    link: 'fs.blog',
+  },
+  {
+    id: 2,
+    tags: ['books', 'david foster wallace', 'education'],
+    title: 'Co-WIN Application',
+    link: 'selfregistration.cowin.gov.in',
+  },
+  {
+    id: 3,
+    tags: ['google', 'google search', 'list'],
+    title: '20 Google Search Tips to Use Google More Efficiently - Lifehack',
+    link: 'www.lifehack.org',
+  },
+];
+export const people_also_read = [
+  {
+    id: 0,
+    title: 'Free Note-Taking App For Connecting Ideas | The Startup',
+    siteAdrress: 'medium.com',
+    tags: [],
+  },
+  {
+    id: 1,
+    title: 'OneNote, digital note taking app - Office',
+    siteAdrress: 'www.microsoft.com',
+    tags: ['digital note taking', 'note taking', 'onenote'],
+  },
+  {
+    id: 2,
+    title: '20 macOS Shortcuts to Make You Work Like a Genius',
+    siteAdrress: 'www.entrepreneur.com',
+    tags: ['apple', 'gadgets', 'hacks', 'productivity'],
+  },
+  {
+    id: 3,
+    title: 'Things vs OmniFocus vs Todoist: A comparison of the best ...',
+    siteAdrress: 'thesweetsetup.com',
+    tags: [],
+  },
+];
+export const more_like_this = [
+  {
+    id: 1,
+    title: 'Academic Workflow - Any Suggestion for an ... - Outliner Software',
+    description: '',
+    linkimg: '',
+    link: 'www.outlinersoftware.com',
+  },
+  {
+    id: 2,
+    title: 'Editorial for iOS - omz:software',
+    description:
+      'Editorial is a plain text editor for iOS with great Markdown support and powerful automation tools. Make yourself at home with custom workflows, snippets, ...',
+    linkimg: '',
+    link: 'omz-software.com',
+  },
+  {
+    id: 3,
+    description: 'How do you export an outline of actions to Omni Outliner ...',
+    linkimg: '',
+    link: 'www.outlinersoftware.com',
+  },
+];
+export const myHightlight = [
+  {
+    id: 1,
+    title: 'Tips and Tricks for Distraction-Free Writing - Lifehack',
+    siteImg:
+      'https://www.eadeverell.com/wp-content/uploads/googledocs-800x1150.jpg',
+    myTags: ['product'],
+    my_highlight: [
+      [
+        'yellow',
+        `Google Docs is a great alternative to Microsoft Word. If you’re a writer, I think you’ll find the mobile apps and collaboration options particularly useful, and you may be surprised by some of Google Docs’ lesser-known features too.`,
+      ],
+      [
+        'yellow',
+        'This is by far my favourite thing about Google Docs, as anyone who’s participated in my write-alongs or watched my live writing knows. :) You can share a link with anyone you want, and they can watch the words appear in the document as you type them. If you’re a writer, this is a fun way to give readers a glimpse into the composition, or (best of all, in my opinion), alleviate the solitude of the writing process. By the way, I usually send out a tweet, or an email to my Coterie when I’m live writing, so follow me if you’re interested.',
+      ],
+      [
+        'blue',
+        'I’ve found work very nicely. When you’re collaborating with a group of readers or writers on a project, it’s a huge boon to know that no one will need to download special software and end up encountering compatibility or versioning issues.',
+      ],
+    ],
+    link: 'www.eadeverell.com',
+    linkImg:
+      'https://www.eadeverell.com/wp-content/uploads/2013/10/pic3-200x200.jpg',
+    date: 'Jul 4, 2021',
+  },
+  {
+    id: 2,
+    title: 'Writing Apps for Bloggers',
+    siteImg: '',
+    myTags: ['product'],
+    my_highlight: [
+      ['yellow', 'Although you can write your articles in Microsoft Word'],
+    ],
+    link: 'www.lifehack.org',
+    linkimg:
+      'https://cdn.lifehack.org/wp-content/uploads/2008/01/20081013-writing.png',
+    date: 'Jul 1, 2021',
+  },
+  {
+    id: 3,
+    title: 'LINER | Icons As Part Of A Great User Experience — Smashing ...',
+    siteImg: 'https://getliner.com/landing/image/og_image.jpg',
+    myTags: [],
+    my_highlight: [
+      [
+        'yellow',
+        'Another common technique is to use tutorials or coach marks or popover hints. However, users might simply rush through the tutorial or forget everything they’ve learned when they next launch your app. Like tooltips, tutorials are no substitute for intuitive design; rather, the opposite. As Matthew at CocoaLove says, “Your app’s tutorial screen is just a list of ways you’ve failed.”',
+      ],
+    ],
+    link: 'getliner.com',
+    linkimg: 'https://getliner.com/src/images/favicon.svg',
+    date: 'Jul 1, 2021',
+  },
+  {
+    id: 4,
+    title: 'Tips and Tricks for Distraction-Free Writing - Lifehack',
+    siteImg:
+      'https://cdn.lifehack.org/wp-content/uploads/2008/01/20081013-writing.png',
+    myTags: [],
+    my_highlight: [],
+    link: 'www.lifehack.org',
+    linkimg: 'https://getliner.com/src/images/favicon.svg',
+    date: 'Jul 1, 2021',
+  },
+];
