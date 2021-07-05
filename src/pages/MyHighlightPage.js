d32e4c4a48ea638a44ecb828f85d37de445d4aa5
성현Maria 김 <globalkmaria@gmail.com>
2021-07-06 01:10:52 +0900

    finished my highlight page

--- src/pages/MyHighlightPage.js
+++ src/pages/MyHighlightPage.js
@@ -1,17 +1,19 @@
 import React from 'react';
+import MySummery from '../container/main/MySummery';
 import MyTitle from '../container/main/MyTitle';
 import LinerMore from '../container/right_menu/LinerMore';
+import PeopleAlso from '../container/right_menu/PeopleAlso';
 
 function MyHighlightPage() {
   return (
     <main className="middle-container">
       <div className="main">
         <MyTitle />
-        {/* <MySummery /> */}
+        <MySummery />
       </div>
       <div className="rightMenu">
         <div className="rightMenu-container">
-          {/* <PeoplePages /> */}
+          <PeopleAlso />
           <LinerMore />
         </div>
       </div>
