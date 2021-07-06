b6799d7ee4ca67e4427f3fe27ff65613f89567af
성현Maria 김 <globalkmaria@gmail.com>
2021-07-06 15:42:46 +0900

    recommand data update

--- src/data.js
+++ src/data.js
@@ -3,6 +3,7 @@ export const detail_data = [
     id: 1,
     title: 'OneNote vs. Evernote: A personal take on two great note ...',
     siteTags: [],
+
     moreInfo: [
       [
         'Description',
@@ -10,8 +11,12 @@ export const detail_data = [
       ],
       ['Authors', 'Preston Gralla'],
     ],
-    liner_highlight:
-      'However, the company changed course in late 2019, announcing that it will continue to add new features to desktop version of OneNote and that the desktop version will once again be included with Office 365 as of March 2020',
+    liner_highlight: [
+      [
+        'blue',
+        'However, the company changed course in late 2019, announcing that it will continue to add new features to desktop version of OneNote and that the desktop version will once again be included with Office 365 as of March 2020',
+      ],
+    ],
     siteImg: '/assests/site_img/site1.jpeg',
     linkImg: '/assests/link_img/link1.png',
     link: 'www.computerworld.com',
@@ -27,8 +32,12 @@ export const detail_data = [
       ],
       ['Authors', 'Lydia Belanger'],
     ],
-    liner_highlight:
-      'In Finder, you can secondary click a file and assign it a color “Tag.” When you click that Tag on the left sidebar of Finder, all folders or files with that Tag will appear. You can also drag a Tag group (what’s called a "Stack") from the Finder sidebar down to your Dock (row of icons) for easier access',
+    liner_highlight: [
+      [
+        'blue',
+        'In Finder, you can secondary click a file and assign it a color “Tag.” When you click that Tag on the left sidebar of Finder, all folders or files with that Tag will appear. You can also drag a Tag group (what’s called a "Stack") from the Finder sidebar down to your Dock (row of icons) for easier access',
+      ],
+    ],
     siteImg: '/assests/site_img/site2.jpeg',
     linkImg: '/assests/link_img/link2.jpeg',
     link: 'www.entrepreneur.com',
@@ -44,8 +53,12 @@ export const detail_data = [
       ],
       ['Authors', 'http://facebook.com/ladywriters'],
     ],
-    liner_highlight:
-      'Update: Google Docs now has a built-in outliner. When you open it from Tools > Document Outline View > Show document outline, it will pick up any heading ...',
+    liner_highlight: [
+      [
+        'blue',
+        'Update: Google Docs now has a built-in outliner. When you open it from Tools > Document Outline View > Show document outline, it will pick up any heading ...',
+      ],
+    ],
     siteImg:
       'https://www.eadeverell.com/wp-content/uploads/googledocs-800x1150.jpg',
     linkImg:
@@ -62,8 +75,12 @@ export const detail_data = [
         '10 Oct 2020. tags: spaced repetition roam. The Delta (Δ) function in Roam is ... The Delta (Δ) function in Roam is probably the most powerful feature in Roam ...',
       ],
     ],
-    liner_highlight:
-      'How I think of it is that it works kind of like time travel with clones. When you click on the Δ button for a block, the original block is sent forward in time. In its place a “clone” is created. This clone is basically a copy of the original block that links back to the original. If you have used the Δ button once for a given block, the links...',
+    liner_highlight: [
+      [
+        'blue',
+        'How I think of it is that it works kind of like time travel with clones. When you click on the Δ button for a block, the original block is sent forward in time. In its place a “clone” is created. This clone is basically a copy of the original block that links back to the original. If you have used the Δ button once for a given block, the links...',
+      ],
+    ],
     siteImg: '',
     linkImg: 'https://getliner.com/src/images/favicon.svg',
     link: 'www.iantay.dev',
@@ -79,7 +96,12 @@ export const detail_data = [
         'Where I answer questions . Contribute to BryanJenksCommunity/FAQ development by creating an account on GitHub.',
       ],
     ],
-    liner_highlight: `The way I build MOC's with the [[tags]] links that don't exist mean I don't need to manage/track anything in regards to promoting a [[tag]] to a MOC I can just see a big red node on my graph, it's VISUALLY apparent`,
+    liner_highlight: [
+      [
+        'blue',
+        `The way I build MOC's with the [[tags]] links that don't exist mean I don't need to manage/track anything in regards to promoting a [[tag]] to a MOC I can just see a big red node on my graph, it's VISUALLY apparent`,
+      ],
+    ],
     siteimg: 'https://github.com/opengraph.png',
     linkImg: 'https://github.githubassets.com/favicons/favicon.svg',
     link: 'github.com',
@@ -148,23 +170,50 @@ export const more_like_this = [
   {
     id: 1,
     title: 'Academic Workflow - Any Suggestion for an ... - Outliner Software',
-    description: '',
-    linkimg: '',
+    moreInfo: [],
+    liner_highlight: [
+      [
+        'blue',
+        'However, the company changed course in late 2019, announcing that it will continue to add new features to desktop version of OneNote and that the desktop version will once again be included with Office 365 as of March 2020',
+      ],
+    ],
+    siteImg:
+      'https://cdn.zapier.com/storage/blog/b59f84994ba6b8e7dd010a0038abc9a0.1800x1000.jpg',
+    linkImg:
+      'https://i1.wp.com/fortelabs.co/wp-content/uploads/2020/02/cropped-cropped-Icon_Red-1.png?fit=192%2C192&ssl=1',
     link: 'www.outlinersoftware.com',
+    siteTags: ['git'],
   },
   {
     id: 2,
-    title: 'Editorial for iOS - omz:software',
-    description:
-      'Editorial is a plain text editor for iOS with great Markdown support and powerful automation tools. Make yourself at home with custom workflows, snippets, ...',
-    linkimg: '',
+    title: 'Just-In-Time PM #9: Placeholders - Forte Labs',
+    liner_highlight: [
+      [
+        'blue',
+        'Editorial is a plain text editor for iOS with great Markdown support and powerful automation tools. Make yourself at home with custom workflows, snippets, ...',
+      ],
+    ],
+    siteImg:
+      'https://wac-cdn.atlassian.com/dam/jcr:d88c7ba7-793a-42e9-9fe7-1b96dbe9acb8/Team-Playbook-Facebook-1200x630.png',
+    linkImg:
+      'https://www.eadeverell.com/wp-content/uploads/2013/10/pic3-200x200.jpg',
     link: 'omz-software.com',
+    siteTags: ['apple', 'gadgets', 'hacks', 'productivity', 'technology'],
+    moreInfo: [],
   },
   {
     id: 3,
-    description: 'How do you export an outline of actions to Omni Outliner ...',
-    linkimg: '',
+    title: 'Editorial for iOS - omz:software',
+    liner_highlight: [
+      ['blue', 'How do you export an outline of actions to Omni Outliner ...'],
+    ],
+    siteImg:
+      'https://www.eadeverell.com/wp-content/uploads/googledocs-800x1150.jpg',
+    linkImg:
+      'https://wac-cdn.atlassian.com/assets/img/favicons/atlassian/favicon.png',
     link: 'www.outlinersoftware.com',
+    siteTags: [],
+    moreInfo: [],
   },
 ];
 export const myHightlight = [
@@ -192,6 +241,7 @@ export const myHightlight = [
     linkImg:
       'https://www.eadeverell.com/wp-content/uploads/2013/10/pic3-200x200.jpg',
     date: 'Jul 4, 2021',
+    moreInfo: [],
   },
   {
     id: 2,
@@ -204,6 +254,13 @@ export const myHightlight = [
     link: 'www.lifehack.org',
     linkImg: 'https://getliner.com/src/images/favicon.svg',
     date: 'Jul 1, 2021',
+    moreInfo: [
+      [
+        'Description',
+        'Update: Google Docs now has a built-in outliner. When you open it from Tools > Document Outline View > Show document outline, it will pick up any heading ...',
+      ],
+      ['Authors', 'http://facebook.com/ladywriters'],
+    ],
   },
   {
     id: 3,
@@ -219,7 +276,9 @@ export const myHightlight = [
     link: 'getliner.com',
     linkImg: 'https://getliner.com/src/images/favicon.svg',
     date: 'Jul 1, 2021',
+    moreInfo: [],
   },
+
   {
     id: 4,
     title: 'Tips and Tricks for Distraction-Free Writing - Lifehack',
@@ -230,5 +289,6 @@ export const myHightlight = [
     link: 'www.lifehack.org',
     linkImg: 'https://getliner.com/src/images/favicon.svg',
     date: 'Jul 1, 2021',
+    moreInfo: [],
   },
 ];
